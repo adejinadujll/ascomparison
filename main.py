4e9f57d0d59639199e027042c8eb0997e64c913c
adejinadujll <111998217+adejinadujll@users.noreply.github.com>
2022-11-08 17:57:57 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -7,58 +7,80 @@ st.markdown(""" <style> .font {
 font-size:25px ; font-family: 'Cooper Black'; color: #FF9633;} 
 </style> """, unsafe_allow_html=True)
 
-def find_new_additions(df,df1):
-    
-    file_1_ids = df['Advert ID'].to_list()
-    file_2_ids = df1['Advert ID'].to_list()
-    
-    new_records = (list(set(file_2_ids) - set(file_1_ids)))
-    
-    new_file = (df1.loc[df1['Advert ID'].isin(new_records)])
-    
-    new_file = new_file.set_index("Advert ID")
-    
-    return(new_file)
-    
-    
-def no_longer_listed(df,df1):
-    
-    file_1_ids = df['Advert ID'].to_list()
-    file_2_ids = df1['Advert ID'].to_list()
-    
-    missing_records = (list(set(file_1_ids) - set(file_2_ids)))
-    
-    new_file = (df.loc[df['Advert ID'].isin(missing_records)])
-    
-    new_file = new_file.set_index("Advert ID")
-    
-    return(new_file)
-    
-    
-def compare_existing_rows(df,df1):
-    
-    df = df.drop(['Details Last Confirmed'], axis=1)
-    df1 = df1.drop(['Details Last Confirmed'], axis=1)
-    
-    file_1_ids = df['Advert ID'].to_list()
-    file_2_ids = df1['Advert ID'].to_list()
-    
-    existing_records = (list(set(file_1_ids).intersection(file_2_ids)))
+tabs_font_css = """
+<style>
+button[data-baseweb="tab"] {
+    font-size: 20px;
+}
+</style>
+"""
 
-    new_file1 = (df.loc[df['Advert ID'].isin(existing_records)])
-    new_file2 = (df1.loc[df1['Advert ID'].isin(existing_records)])
+title_font_css = """
+<style>
+button[data-baseweb="tab"] {
+    font-size: 20px;
+}
+</style>
+"""
+
+def find_new_additions(platform_options,df,df1):
     
-    df = pd.concat([new_file1, new_file2])
-    df = df.reset_index(drop=True)
-    df_gpby = df.groupby(list(df.columns))
+    if platform_options == "Agents Society":
+        
+        file_1_ids = df['Advert ID'].to_list()
+        file_2_ids = df1['Advert ID'].to_list()
+        
+        new_records = (list(set(file_2_ids) - set(file_1_ids)))
+        
+        new_file = (df1.loc[df1['Advert ID'].isin(new_records)])
+        
+        new_file = new_file.set_index("Advert ID")
     
-    idx = [x[0] for x in df_gpby.groups.values() if len(x) == 1]
+        return(new_file)
     
-    changed = df.reindex(idx)
     
-    changed = changed.set_index("Advert ID")
+def no_longer_listed(platform_options,df,df1):
     
-    return(changed)
+    if platform_options == "Agents Society":
+    
+        file_1_ids = df['Advert ID'].to_list()
+        file_2_ids = df1['Advert ID'].to_list()
+        
+        missing_records = (list(set(file_1_ids) - set(file_2_ids)))
+        
+        new_file = (df.loc[df['Advert ID'].isin(missing_records)])
+        
+        new_file = new_file.set_index("Advert ID")
+        
+        return(new_file)
+    
+   
+def compare_existing_rows(platform_options,df,df1):
+    
+    if platform_options == "Agents Society":
+    
+        df = df.drop(['Details Last Confirmed'], axis=1)
+        df1 = df1.drop(['Details Last Confirmed'], axis=1)
+        
+        file_1_ids = df['Advert ID'].to_list()
+        file_2_ids = df1['Advert ID'].to_list()
+        
+        existing_records = (list(set(file_1_ids).intersection(file_2_ids)))
+
+        new_file1 = (df.loc[df['Advert ID'].isin(existing_records)])
+        new_file2 = (df1.loc[df1['Advert ID'].isin(existing_records)])
+        
+        df = pd.concat([new_file1, new_file2])
+        df = df.reset_index(drop=True)
+        df_gpby = df.groupby(list(df.columns))
+        
+        idx = [x[0] for x in df_gpby.groups.values() if len(x) == 1]
+        
+        changed = df.reindex(idx)
+        
+        changed = changed.set_index("Advert ID")
+        
+        return(changed)
 
 @st.cache
 def convert_df(df):
@@ -67,7 +89,7 @@ def convert_df(df):
 
  
                    
-st.header("Agents Society Comparison Tool")
+st.title("Agents Society Comparison Tool")
 
 tabs_font_css = """
 <style>
@@ -77,38 +99,41 @@ button[data-baseweb="tab"] {
 </style>
 """
 
-st.write(tabs_font_css, unsafe_allow_html=True)
-tab1, tab2, tab3 = st.tabs(["   Updated Records  ", "   New Records Created   ", "   Removed Records   "])
 
-with tab1:
-    
-    st.write("Use this tab to identify records which have been recently updated.")
-    st.caption("Remove all special characters from .csv files to avoid upload errors.")
-    
-    st.subheader("Upload Files")
-    
+platform_options = st.multiselect(
+    label = "Select Platform",
+    options = ['Agents Society'],
+    )
+
+if platform_options :
+       if len(platform_options) > 1:
+
+        st.warning("Please select 1 platform.")
+
+if len(platform_options) == 1:
+   
     try:
-        uploaded_file_1 = st.file_uploader(label="Upload Initial AS Extract (.csv)",key="upload1")
+        uploaded_file_1 = st.file_uploader(label="Upload Initial File", key="upload1")
         
         if uploaded_file_1 is not None:
-        
-            df = pd.read_csv(uploaded_file_1,encoding="unicode_escape")
             
+            df = pd.read_csv(uploaded_file_1,encoding="unicode_escape")
+                  
             with st.expander("See Uploaded Data"):
             
                 st.write(df)
-                
+                    
     except:
         
         st.write("Upload error.")
-    
+
     try:
-        uploaded_file_2 = st.file_uploader(label="Upload Latest AS Extract (.csv)",key="upload2")
+        uploaded_file_2 = st.file_uploader(label="Upload Comparison File",key="upload2")
         
         if uploaded_file_2 is not None:
         
             df1 = pd.read_csv(uploaded_file_2,encoding="unicode_escape")
-            
+
             with st.expander("See Uploaded Data"):
             
                 st.write(df1)
@@ -116,132 +141,67 @@ with tab1:
     except:
         
         st.write("Upload error.")
-    
-    st.subheader("Results")
-            
+        
     if uploaded_file_1 is not None and uploaded_file_2 is not None:
-        
-        result = compare_existing_rows(df,df1)
 
-        st.write(result)
-        
-        res = convert_df(result)
-        
-        if res:
-            
-            st.download_button(
-            label="Download results as CSV",
-            data=res,
-            file_name='Missing records.csv',
-            mime='text/csv',)
-        
-with tab2:
-    
-    st.write("Use this tab to identify records which have been recently added.")
-    st.caption("Remove all special characters from .csv files to avoid upload errors.")
-    
-    st.subheader("Upload Files")
-    
-    try:
-        
-        uploaded_file_2 = st.file_uploader(label="Upload Initial AS Extract (.csv)",key="upload3")
-        
-        if uploaded_file_2 is not None:
-        
-            df = pd.read_csv(uploaded_file_2,encoding="unicode_escape")
-            
-            with st.expander("See Uploaded Data"):
-            
-                st.write(df)
-    
-    except:
-        
-        st.write("Upload error.")     
-    
-    try:
-        uploaded_file_3 = st.file_uploader(label="Upload Latest AS Extract (.csv)",key="upload4")
-        
-        if uploaded_file_3 is not None:
-        
-            df1 = pd.read_csv(uploaded_file_3,encoding="unicode_escape")
-            
-            with st.expander("See Uploaded Data"):
-            
-                st.write(df1)
-    except:
-        
-        st.write("Upload error.")
-    
-    st.subheader("Results")
-            
-    if uploaded_file_2 is not None and uploaded_file_3 is not None:
-        
-        result = find_new_additions(df,df1)
+        st.write(tabs_font_css, unsafe_allow_html=True)
+        tab1, tab2, tab3 = st.tabs(["   Updated Records  ", "   New Records Created   ", "   Removed Records   "])
 
-        st.write(result)
-        
-        res = convert_df(result)
-        
-        if res:
+        with tab1:
             
-            st.download_button(
-            label="Download results as CSV",
-            data=res,
-            file_name='Missing records.csv',
-            mime='text/csv',)
-        
-with tab3:
-    
-    st.write("Use this tab to identify records which have been recently removed.")
-    st.caption("Remove all special characters from .csv files to avoid upload errors.")
-    
-    st.subheader("Upload Files")
-    
-    try:
-        
-        uploaded_file_4 = st.file_uploader(label="Upload Initial AS Extract (.csv)",key="upload5")
-        
-        if uploaded_file_4 is not None:
-        
-            df = pd.read_csv(uploaded_file_4,encoding="unicode_escape")
-            
-            with st.expander("See Uploaded Data"):
-            
-                st.write(df)
-    except:
-        
-        st.write("Upload error.")
-    
-    try:
-        
-        uploaded_file_5 = st.file_uploader(label="Upload Latest AS Extract (.csv)",key="upload6")
-        
-        if uploaded_file_5 is not None:
-        
-            df1 = pd.read_csv(uploaded_file_5,encoding="unicode_escape")
-            
-            with st.expander("See Uploaded Data"):
-            
-                st.write(df1)
-    except:
-        
-        st.write("Upload error.")
-        
-    st.subheader("Results")
-    
-    if uploaded_file_4 is not None and uploaded_file_5 is not None:
-        
-        result = no_longer_listed(df,df1)
+            st.write("Use this tab to identify records which have been recently updated.")
+                            
+            result = compare_existing_rows(platform_options[0],df,df1)
 
-        st.write(result)
-        
-        res = convert_df(result)
-        
-        if res:
-        
-            st.download_button(
-            label="Download results as CSV",
-            data=res,
-            file_name='Missing records.csv',
-            mime='text/csv',)
+            st.write(result)
+            
+            res = convert_df(result)
+            
+            if res:
+                
+                st.download_button(
+                label="Download results as CSV",
+                data=res,
+                key = 1,
+                file_name='Missing records.csv',
+                mime='text/csv',)
+                
+                
+        with tab2:
+            
+            st.write("Use this tab to identify records which have been recently added.")
+                
+            result = find_new_additions(platform_options[0],df,df1)
+
+            st.write(result)
+            
+            res = convert_df(result)
+            
+            if res:
+                
+                st.download_button(
+                label="Download results as CSV",
+                data=res,
+                key = 2,
+                file_name='Missing records.csv',
+                mime='text/csv',)
+                
+        with tab3:
+            
+            st.write("Use this tab to identify records which have been recently removed.")
     
+            result = no_longer_listed(platform_options[0],df,df1)
+
+            st.write(result)
+            
+            res = convert_df(result)
+            
+            if res:
+            
+                st.download_button(
+                label="Download results as CSV",
+                data=res,
+                key = 3,
+                file_name='Missing records.csv',
+                mime='text/csv',)
+                
