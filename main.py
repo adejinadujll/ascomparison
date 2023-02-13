84aeea4c4287c5bac8a6ffbb307aba04c8727571
adejinadujll <111998217+adejinadujll@users.noreply.github.com>
2023-02-13 14:10:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import openpyxl 
+import  
 
 st.set_page_config(layout="wide")
 
@@ -175,8 +175,7 @@ if len(platform_options) == 1:
 
         if uploaded_file_1 is not None:
             
-#             df = pd.read_csv(uploaded_file_1,encoding="latin1")
-            df = pd.read_excel(uploaded_file_1,engine = 'openpyxl')
+            df = pd.read_csv(uploaded_file_1,encoding="latin1")
             
             df = remove_false_values(df)
 
@@ -193,8 +192,7 @@ if len(platform_options) == 1:
        
         if uploaded_file_2 is not None:
         
-#             df1 = pd.read_csv(uploaded_file_2,encoding="latin1")
-            df1 = pd.read_excel(uploaded_file_2)
+            df1 = pd.read_csv(uploaded_file_2,encoding="latin1")
             
             df1 = remove_false_values(df1)
 
